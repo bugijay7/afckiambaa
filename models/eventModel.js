dcc88bee71ee55e68683f3e2f5ca259f79cd03be
Kibugi Maina <bugijay7@gmail.com>
2025-11-17 19:30:44 +0300

    eventModel update

--- models/eventModel.js
+++ models/eventModel.js
@@ -4,10 +4,11 @@ const eventSchema = new mongoose.Schema({
   title: { type: String, required: true },
   date: { type: Date, required: true },
   description: { type: String, required: true },
+  location: { type: String, required: true }, // ✅ Added
   image: {
     url: { type: String, required: true },
     public_id: { type: String, required: true },
   },
-});
+}, { timestamps: true }); // optional: track createdAt & updatedAt
 
 export default mongoose.model('Event', eventSchema);
