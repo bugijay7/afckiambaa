6f0fac941221f1aaaa90ad58663fb775b3543ef5
Kibugi Maina <bugijay7@gmail.com>
2025-11-21 14:29:32 +0300

    cloudinary url

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -6,11 +6,8 @@ dotenv.config();
 
 // ✅ Cloudinary config (supports CLOUDINARY_URL or separate keys)
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true, // optional: forces https
 });
-
 // 🟩 CREATE EVENT
 export const createEvent = async (req, res) => {
   try {
